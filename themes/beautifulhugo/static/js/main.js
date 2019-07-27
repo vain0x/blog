3cf40f543e9ecb18470f21af4ea2bac3b34ec592
vain0x <vainzerox@gmail.com>
2019-07-27 22:55:35 +0900

    [theme] ナビゲーションバーを常に縮めた状態にする

--- themes/beautifulhugo/static/js/main.js
+++ themes/beautifulhugo/static/js/main.js
@@ -6,15 +6,6 @@ var main = {
   numImgs : null,
 
   init : function() {
-    // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar").addClass("top-nav-short");
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-        }
-    });
-
     // On mobile, hide the avatar when expanding the navbar menu
     $('#main-navbar').on('show.bs.collapse', function () {
       $(".navbar").addClass("top-nav-expanded");
